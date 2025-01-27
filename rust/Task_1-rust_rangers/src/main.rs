eccd45c8a0462f4828d05741d469c4483d442015
mitanshu-kelaiya-inventyv <mitanshu.kelaiya.inventyv@gmail.com>
2025-01-27 10:35:39 +0530

    rand dependency added

--- rust/Task_1-rust_rangers/src/main.rs
+++ rust/Task_1-rust_rangers/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     // levels::_1_level_one::start_level_1();
     // levels::_2_level_two::start_level_2();
     // levels::_3_level_three::start_level_3();
-    // levels::_4_level_four::start_level_4();
+    levels::_4_level_four::start_level_4();
     // levels::_5_level_five::start_level_5();
 }
 
